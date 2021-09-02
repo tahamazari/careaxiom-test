8d1575f5ed54bf271e80dadac25038aae9eca17c
tahamazari <tahamazari@gmail.com>
2021-09-02 21:41:44 +0500

    done with async js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const async = require("async");
 
-const { generateHtml, promisesAll } = require("./utils")
+const { generateHtml, getTitle } = require("./utils")
 
 const app = express();
 
@@ -10,19 +11,29 @@ app.get('/I/want/title', async (req, res, next) => {
   let titles = []
   let list = ""
 
-  titles = await Promise.all(websitesArray.map(url => promisesAll(url)))
-  list = titles.join("")
-
-  if(websitesArray.length === 0){
+  if(websitesArray.length === 0 && typeof(websitesArray) !== "string"){
     res.status(200).send("<h1>No urls provided</h1>")
   }
-  else{
-    res.status(200).send(generateHtml(list))
-  }
+
+  const inputUrls = typeof(websitesArray) === "string" ? [websitesArray] : websitesArray
+
+  async.map(inputUrls, async (url) => {
+    let { title, success } = await getTitle(url)
+    if(!success){
+      titles.push(`<li>${url} - No response</li>`)
+    }
+    else{
+      titles.push(`<li>${url} - ${title}</li>`)
+    }
+  }, () => {
+    list = titles.join("")
+    console.log(titles)
+    return res.status(200).send(generateHtml(list))
+  })
 });
 
 app.get('*', function(req, res){
-  res.status(404).send("I don't know how you get here but this place doesn't exist!")
+  res.status(404).send("<h1>I don't know how you get here but this place doesn't exist!</h1>")
 });
 
 app.listen(3000, function () {
